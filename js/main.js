cc2e63d204c9b5f50040acd74718441ddf610ab9
Von Balanon <balanonvon@gmail.com>
2022-06-28 20:12:01 -0700

    added syntax walkthrough

---
+++ js/main.js
@@ -0,0 +1,25 @@
+ 
+ //coment: one line comment
+ /* 
+ this is a mult-line comment
+ */ //ens with */
+
+ // variables
+ 
+ 
+ yes = true; // boolean
+ no = false; 
+ Yes = 10;
+ console.log(yes, Yes);
+
+ count = 0; // number
+ one = 1;
+ two =2;
+ three =3;
+
+ title = 'Hello World'; // string
+
+ document.querySelector('h1').innerHTML= title;
+ document.querySelector('p').innerHTML = count;
+
+ console.log(title);
