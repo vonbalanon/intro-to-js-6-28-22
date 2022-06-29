b31a2dff1d1241857f55b23ff32f3e158a0f6fa1
Von Balanon <balanonvon@gmail.com>
2022-06-28 20:59:08 -0700

    update

--- js/main.js
+++ js/main.js
@@ -6,11 +6,26 @@
  i like cheese
  */ 
 
- title = 'Von'; // string
- paragraph = 'Chicken nuggets';
+
+yes = true;
+no = false;
+Yes = 10;
+Name='Von';
+Food='Chicken Nuggets'
+console.log (Name)
+console.log (Food)
+ console.log (yes, Yes, 2, 3, 4);
+
+ count = 0;
+ one=1;
+ two=2;
+ three=3;
+
+
+ title = 'Hello World'; // string
 
  document.querySelector('h1').innerHTML= title;
- document.querySelector('p').innerHTML = paragraph;
+ document.querySelector('p').innerHTML = count;
 
+ 
  console.log(title);
- console.log(paragraph)
